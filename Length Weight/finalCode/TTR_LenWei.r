1283a25887aad2baa0eb6b1fb5e02d9181a459d0
milokmilo <milokmilo@gmail.com>
2015-04-17 17:37:43 +0200

    Length-Weight revised, axis changed, two models applied and plots updated

--- Length Weight/finalCode/TTR_LenWei.r
+++ Length Weight/finalCode/TTR_LenWei.r
@@ -1,11 +1,11 @@
-####################################################################################################
+################################################################################################## #
 #                     LENGTH-WEIGHT CURVES FOR BOTLLENOSE DOLPHIN
 #                    Estimate length-weight relationship based on 
 #                     Ridgway and Fenner, 1982 relationship and
 #            age-length and age-weight relationship in Wells and Scott, 1999
 #             created: C.Saavedra (camilo.saavedra@vi.ieo.es) 09/04/2015
 #             modified: C.Saavedra (camilo.saavedra@vi.ieo.es) 17/04/2015
-####################################################################################################
+################################################################################################## #
 
 # inputs
 # "../../RData/MaleTTR-AgeWeight.txt
@@ -20,10 +20,13 @@
 
 # IMPORTANT: Set working directory (to source file location)
 #setwd("./Length weight/finalCode")
-#setwd("../..//Length weight/finalCode")
+#setwd("../../Length weight/finalCode")
 
 # Charge libraries
 library(ggplot2)
+mytheme <- theme(panel.background =  element_rect(fill = NA, colour = "black", size = 0.5), 
+                 legend.title= element_blank(), legend.position="top")
+
 
 
 ###########     Length-Weight relationship  (Ridgway and Fenner, 1982)    #############
@@ -34,82 +37,90 @@ library(ggplot2)
 
   # All
 
-xAll <- c(188,259)
-yAll <- c(85,184)
-Alm <- lm(yAll ~ xAll)
-All <- data.frame(x=xAll, y=yAll)
-#Allem <- nls(y ~ exp(a + b * x), data = All, start = list(a = 2.7, b = 0.01))
-All <- data.frame(xAll=90:350)
-All$yAll <- predict(Alm, newdata=All)
-#All$yAll <- predict(Allem, newdata=All)
-plot(xAll,yAll,ylim=c(0,300),xlim=c(90,350))
+LAll <- c(188,259)
+WAll <- c(85,184)
+Alm <- lm(WAll ~ LAll)
+All <- data.frame(L=LAll, W=WAll)
+#Allem <- nls(W ~ exp(a + b * L), data = All, start = list(a = 2.7, b = 0.01))
+All <- data.frame(LAll=90:350)
+All$WAll <- predict(Alm, newdata=All)
+#All$WAll <- predict(Allem, newdata=All)
+plot(LAll,WAll,ylim=c(0,300),xlim=c(90,350))
 lines(All)
 
 
   # Males:
 
-xMales <- c(187,195,200,202,213,217,217,222,227,230,247,243)
-yMales <- c(93,101,103,99,116,119,118,121,132,158,158,182)
-#Malm <- lm(yMales ~ xMales)
-M <- data.frame(xMales=xMales, yMales=yMales)
-Mem <- nls(yMales ~ exp(a + b * xMales), data = M, start = list(a = 4, b = 0.003))
-Males <- data.frame(xMales=90:350)
-#Males$yMales <- predict(Malm, newdata=Males)
-Males$yMales <- predict(Mem, newdata=Males)
-plot(xMales,yMales,ylim=c(0,300),xlim=c(90,350))
-lines(Males)
-summary(Fem)
-coef(Fem)
-summary(Mem)
-coef(Mem)
+LMales <- c(187,195,200,202,213,217,217,222,227,230,247,243)
+WMales <- c(93,101,103,99,116,119,118,121,132,158,158,182)
+M <- data.frame(LMales=LMales, WMales=WMales)
+Mem <- nls(WMales ~ exp(a + b * LMales), data = M, start = list(a = 4, b = 0.003))
+Males <- data.frame(LMales=90:350)
+Males$WMales <- predict(Mem, newdata=Males)
+#plot(LMales,WMales,ylim=c(0,300),xlim=c(90,350))
+#lines(Males)
+#summary(Mem)
+#coef(Mem)
 # a          b 
 # 2.38196812 0.01119829 
 
 
   # Females:
 
-xFemales <- c(194,205,205,207,223,224,228,219,239,223,234,249,250,244,252,256)
-yFemales <- c(98,110,105,105,118,121,131,143,142,171,157,184,173,158,161,155)
-#Femalm <- lm(yFemales ~ xFemales)
-F <- data.frame(xFemales=xFemales, yFemales=yFemales)
-Fem <- nls(yFemales ~ exp(a + b * xFemales), data = F, start = list(a = 4, b = 0.003))
-Females <- data.frame(xFemales=90:350)
-#Females$yFemales <- predict(Femalm, newdata=Females)
-Females$yfemales <- predict(Fem, newdata=Females)
-plot(xFemales,yFemales,ylim=c(0,300),xlim=c(90,350))
-lines(Females)
-summary(Fem)
-coef(Fem)
-#  a           b 
-#  2.971140356 0.008564414 
+LFemales <- c(194,205,205,207,223,224,228,219,239,223,234,249,250,244,252,256)
+WFemales <- c(98,110,105,105,118,121,131,143,142,171,157,184,173,158,161,155)
+F <- data.frame(LFemales=LFemales, WFemales=WFemales)
+Fem <- nls(WFemales ~ exp(a + b * LFemales), data = F, start = list(a = 4, b = 0.003))
+Females <- data.frame(LFemales=90:350)
+Females$Wfemales <- predict(Fem, newdata=Females)
+#plot(LFemales,WFemales,ylim=c(0,300),xlim=c(90,350))
+#lines(Females)
+#summary(Fem)
+#coef(Fem)
+# a           b 
+# 2.971140356 0.008564414 
 
 
   # All2
 
-xAll2 <- c(xMales,xFemales)
-yAll2 <- c(yMales,yFemales)
-A <- data.frame(xAll2=xAll2, xyAll2=yAll2)
-Aem <- nls(yAll2 ~ exp(a + b * xAll2), data = A, start = list(a = 4, b = 0.003))
-All2 <- data.frame(xAll2=90:350)
-#All2$yAll2 <- predict(Alm2, newdata=All2)
-All2$yAll2 <- predict(Aem, newdata=All2)
-plot(xAll2,yAll2,ylim=c(0,300),xlim=c(90,350))
-lines(All2)
-summary(Aem)
-coef(Aem)
-#  a           b 
-#  2.763609038 0.009463226 
+LAll2 <- c(LMales,LFemales)
+WAll2 <- c(WMales,WFemales)
+A <- data.frame(LAll2=LAll2, WAll2=WAll2)
+Aem <- nls(WAll2 ~ exp(a + b * LAll2), data = A, start = list(a = 4, b = 0.003))
+All2 <- data.frame(LAll2=90:350)
+#All2$WAll2 <- predict(Alm2, newdata=All2)
+All2$WAll2 <- predict(Aem, newdata=All2)
+#plot(LAll2,WAll2,ylim=c(0,300),xlim=c(90,350))
+#lines(All2)
+#summary(Aem)
+#coef(Aem)
+# a           b 
+# 2.763609038 0.009463226 
 
 
-###
-plot(xAll2,yAll2,ylim=c(0,450),xlim=c(90,350))
-points(xMales, yMales, col="blue")
-points(xFemales, yFemales, col="red")
-#lines(All)
-lines(All2,col="grey")
-lines(Males,col="blue")
-lines(Females,col="red")
-###
+### Plotting Length Weight following Ridgway and Fenner 1982
+
+ggA <- data.frame(LAll2,WAll2)
+names(ggA) <- c("Length", "Weight")
+names(All2)  <- c("Length", "Weight")
+Males$sex <- "M"
+names(Males)  <- c("Length", "Weight", "Sex")
+Females$sex <- "F"
+names(Females)  <- c("Length", "Weight", "Sex")
+ggMF <- rbind(Males, Females)
+
+
+LW <- ggplot(ggA, aes(Length, Weight)) + geom_point() + xlim(90,300) + ylim(0,350) +
+            geom_line(data=All2, col="black") + 
+            geom_line(data=ggMF, aes(x=Length, y=Weight, colour=factor(Sex))) +
+                      scale_colour_manual(values=c("orangered", "deepskyblue")) +
+            labs(x="Length", y="Weight \n", 
+                 title="Bottlenose dolphin Length-weight relationship following Ridgway and Fenner 1982") +
+            mytheme
+
+png("../plots/TTR_LengthWeight-Ridgway&Fenner.png")
+print(LW)
+dev.off()  
 
 
 ###########     Length-Weight relationship  (Wells and Scott, 1999)    #############
@@ -121,9 +132,11 @@ lines(Females,col="red")
 
 TTR.WM <- read.table("../../RData/MaleTTR-AgeWeight.txt", comment.char="#", head=T)
 range(TTR.WM$Y)
+names(TTR.WM) <- c("age", "weight")
 # [1]  62.3023 288.9056
 TTR.WF <- read.table("../../RData/FemaleTTR-AgeWeight.txt", comment.char="#", head=T)
 range(TTR.WF$Y)
+names(TTR.WF) <- c("age", "weight")
 # [1]  71.40549 233.76414
 
 
@@ -134,84 +147,87 @@ parVB <- list(Linf=290, K=0.18, to=-3)
 
 # Predict values from von Bertalanffy model for age/length
 load(file="../../RObjects/vbM.RData")
-MnewVB <- data.frame(X=seq(1,50,length.out=491)) 
-MnewVB$Y <- predict(vbM,newdata=MnewVB)  
-plot(x=MnewVB$X,y=MnewVB$Y,ylim=c(150,300),xlim=c(0,50), type="l")
+ALMales <- data.frame(age=seq(0,50,length.out=51)) 
+ALMales$length <- predict(vbM, newdata=ALMales)  
+#plot(x=ALMales$age,y=ALMales$length,ylim=c(100,300),xlim=c(0,50), type="l")
 
 # Fit von Bertalanffy model for age/weight and predict values
-vbWM <- nls(Y~Linf*(1-exp(-K*(X-to))), start=parVB, data=TTR.WM) 
-MWnewVB <- data.frame(X=seq(1,50,length.out=491)) 
-MWnewVB$Y <- predict(vbWM,newdata=MWnewVB) 
-plot(x=MWnewVB$X,y=MWnewVB$Y,ylim=c(50,300),xlim=c(0,50), type="l")
+vbWM <- nls(weight ~ Linf*(1-exp(-K*(age-to))), start=parVB, data=TTR.WM) 
+AWMales <- data.frame(age=seq(0,50,length.out=51)) 
+AWMales$weight <- predict(vbWM, newdata=AWMales) 
+#plot(x=AWMales$age,y=AWMales$weight,ylim=c(50,250),xlim=c(0,50), type="l")
 
 # Fit exponential model for length/weight and predict values
-names(MnewVB) <- c("age", "length")
-MnewVB$weight <- MWnewVB$Y
-MnewVB$sex <- "M"
-Mlm <- nls(weight ~ exp(a + b * length), data=MnewVB, start = list(a = 4, b = 0.003))
-coef(Mlm)
+ALWMales <- cbind(ALMales, weight=AWMales$weight)
+Mem <- nls(weight ~ exp(a + b * length), data=ALWMales, start = list(a = 4, b = 0.003))
+coef(Mem)
 # a          b 
-# 1.4900499 0.0152306 
-Mnew <- data.frame(length=seq(90,300)) 
-Mnew$weight <- predict(Mlm,newdata=Mnew) 
-plot(x=MnewVB$length,y=MnewVB$weight,xlim=c(90,300),ylim=c(0,350))
-lines(Mnew, col="blue")
+# 1.50137935 0.01518888 
+Mmod <- data.frame(length=seq(90,300)) 
+Mmod$weight <- predict(Mem,newdata=Mmod) 
+#plot(x=ALWMales$length,y=ALWMales$weight,xlim=c(100,300),ylim=c(0,350))
+#lines(Mmod, col="blue")
+
 
   # Females
 
 # Predict values from von Bertalanffy model for age/length
 load(file="../../RObjects/vbF.RData")
-FnewVB <- data.frame(X=seq(1,50,length.out=491)) 
-FnewVB$Y <- predict(vbF,newdata=FnewVB)
-plot(x=FnewVB$X,y=FnewVB$Y,ylim=c(150,300),xlim=c(0,50), type="l")
+ALFemales <- data.frame(age=seq(0,50,length.out=51)) 
+ALFemales$length <- predict(vbF, newdata=ALFemales)
+#plot(x=ALFemales$age,y=ALFemales$length,ylim=c(100,300),xlim=c(0,50), type="l")
 
 # Fit von Bertalanffy model for age/weight and predict values
-vbWF <- nls(Y~Linf*(1-exp(-K*(X-to))), start=parVB, data=TTR.WF)      
-FWnewVB <- data.frame(X=seq(1,50,length.out=491)) 
-FWnewVB$Y <- predict(vbWF,newdata=FWnewVB) 
-plot(x=FWnewVB$X,y=FWnewVB$Y,ylim=c(50,300),xlim=c(0,50), type="l")
+vbWF <- nls(weight ~ Linf*(1-exp(-K*(age-to))), start=parVB, data=TTR.WF)      
+AWFemales <- data.frame(age=seq(0,50,length.out=51)) 
+AWFemales$weight <- predict(vbWF, newdata=AWFemales) 
+#plot(x=AWFemales$age,y=AWFemales$weight,ylim=c(50,250),xlim=c(0,50), type="l")
 
 # Fit exponential model for length/weight and predict values
-names(FnewVB) <- c("age", "length")
-FnewVB$weight <- FWnewVB$Y
-FnewVB$sex <- "F"
-Flm <- nls(weight ~ exp(a + b * length), data=FnewVB, start = list(a = 4, b = 0.003))
-coef(Flm)
+ALWFemales <- cbind(ALFemales, weight=AWFemales$weight)
+Fem <- nls(weight ~ exp(a + b * length), data=ALWFemales, start = list(a = 4, b = 0.003))
+coef(Fem)
 # a          b 
-# 1.67861312 0.01446434 
-Fnew <- data.frame(length=seq(90,300)) 
-Fnew$weight <- predict(Flm,newdata=Fnew) 
-plot(x=FnewVB$length, y=FnewVB$weight, xlim=c(90,300), ylim=c(0,350))
-lines(Fnew, col="red")
+# 1.78346257 0.01404042
+Fmod <- data.frame(length=seq(100,300)) 
+Fmod$weight <- predict(Fem,newdata=Fmod) 
+#plot(x=ALWFemales$length, y=ALWFemales$weight, xlim=c(100,300), ylim=c(0,350))
+#lines(Fmod, col="red")
+
 
   # All
 
-# Join males and females
-LWAS <- rbind(MnewVB, FnewVB)
+# Joining males and females
+ALWMales$sex <- "M"
+ALWFemales$sex <- "F"
+ALW <- rbind(ALWMales, ALWFemales)
 
 # Fit exponential model for length/weight and predict values
-Alm <- nls(weight ~ exp(a + b * length), data=LWAS, start = list(a = 4, b = 0.003))
-# Coefficients:
-# (Intercept)  log(weight)  
-# -117.03        69.25 
-Anew <- data.frame(length=seq(90,300))
-Anew$weight <- predict(Alm,newdata=Anew)  
+Aem <- nls(weight ~ exp(a + b * length), data=ALW, start = list(a = 4, b = 0.003))
+#coef(Aem)
+#        a          b 
+# 1.54827648 0.01500401
+Amod <- data.frame(length=seq(100,300))
+Amod$weight <- predict(Aem,newdata=Amod)  
+#plot(x=ALW$length, y=ALW$weight, xlim=c(100,300), ylim=c(0,350))
+#lines(Amod, col="green")
 
 
-## Plotting 
 
-Mnew$sex <- "M"
-Fnew$sex <- "F"
-MFnew <- rbind(Mnew,Fnew)
+### Plotting ###
 
-grl <- ggplot(Anew, aes(x=length, y=weight)) + geom_line() +
-  geom_line(data=MFnew, aes(x=length, y=weight, colour=factor(sex))) +
+Mmod$sex <- "M"
+Fmod$sex <- "F"
+MFmod <- rbind(Mmod,Fmod)
+
+grl <- ggplot(Amod, aes(x=length, y=weight)) + geom_line() +
+          geom_point(data=ALW, aes(x=length, y=weight, colour=factor(sex))) +
+          geom_line(data=MFmod, aes(x=length, y=weight, colour=factor(sex))) +
               scale_colour_manual(values=c("orangered", "deepskyblue")) +
-  ggtitle("Bottlenose dolphin length-weight relationship following Wells & Scott, 1999") +
-  xlab("weight (kg)") + ylab("length (cm) \n") +
-  theme(panel.background =  element_rect(fill = NA, colour = "black", size = 0.5), 
-      legend.title= element_blank(), legend.position="top") 
-
+          xlim(100,300) + ylim(0,350) +
+          labs(title="Bottlenose dolphin length-weight relationship following Wells & Scott, 1999",
+          x="weight (kg)", y="length (cm) \n") + mytheme
+ 
 png("../plots/TTR_LengthWeight-Wells&Scott.png", 700, 600)
 print(grl)
 dev.off()
