fb868a4784f0622f1e6f0c4d6c3152bcf7731d25
milokmilo <milokmilo@gmail.com>
2015-04-21 11:41:30 +0200

    Actualizado script para calcular los pesos según la dieta, eliminado el excel de dieta y creado un csv

--- Length Weight/finalCode/TTR_DietWei.r
+++ Length Weight/finalCode/TTR_DietWei.r
@@ -1,45 +1,44 @@
 ####################################################################################################
-#                  ENERGY MODELS ANALYSIS - COMMON DOLPHIN DIET (HAKE)
+#                  ENERGY MODELS ANALYSIS - BOTTLENOSE DOLPHIN DIET (HAKE)
 #                   Build an object with the diet and dolphin weight
-#                    created: (camilo.saavedra@vi.ieo.es) 04/12/2013
+#                   created: (camilo.saavedra@vi.ieo.es) 20/04/2015
 ####################################################################################################
 
 # inputs
 # "../../RData/DDE_AllDiet.csv"
+# "../../RObjects/LWmales.RData"
+# "../../RObjects/LWfemales.RData"
+# "../../RObjects/LWall.RData"
 
 # outputs
-# "../../RObjects/DDE_AllDietWeight.RData"
+# "../../RObjects/TTR_AllDietWeight.RData"
+
 
 # IMPORTANT: Set working directory (to source file location)
+#setwd("./Diet/finalCode")
+#setwd("../../Diet/finalCode")
+
 
 # Read data
-dde <- read.csv("../../RData/DDE_AllDiet.csv")
+ttrDiet <- read.csv2("../../RData/TTR_AllDiet.csv")
+load("../../RObjects/LWmales.RData") # LWmales
+load("../../RObjects/LWfemales.RData") # LWfemales
+load("../../RObjects/LWall.RData") # LWall
+
+ttrDiet$W <- rep(NA,nrow(ttrDiet))
 
 # Adding estimated weight for males, females and indeterminate (three different growth equations)
-for (i in 1:nrow(dde)){
-  if (is.na(dde$Sex[i])) {dde$W[i] <- 10^(-4.68088 + 2.88534 * log10(dde$Length[i]))} else { # all
-    if (dde$Sex[i] == 1) {dde$W[i] <- 10^(-4.56751 + 2.82045 * log10(dde$Length[i]))} else { # males
-      if (dde$Sex[i] == 2) {dde$W[i] <- 10^(-4.74097 + 2.91829 * log10(dde$Length[i]))} else { # females
+for (i in 1:nrow(ttrDiet)){
+  if (is.na(ttrDiet$Length[i])) {ttrDiet$W[i] <- NA} else {
+  if (ttrDiet$Sex[i] == 0) {ttrDiet$W[i] <- exp(LWall[[1]] + LWall[[2]] * ttrDiet$Length[i])} else { # all
+    if (ttrDiet$Sex[i] == 1) {ttrDiet$W[i] <- exp(LWmales[[1]] + LWmales[[2]] * ttrDiet$Length[i])} else { # males
+      if (ttrDiet$Sex[i] == 2) {ttrDiet$W[i] <- exp(LWfemales[[1]] + LWfemales[[2]] * ttrDiet$Length[i])} else { # females
         stop ("Something is wrong")}
     }
   }
-} # Weight estimates for males and females
+  } 
+} # Weight estimated for males, females and for all when sex is not available 
 
-save(file="../../RObjects/DDE_AllDietWeight.RData", dde) 
 
-# Transforming logarithmic in exponential
-# !! FOR ALL DOLPHINS (BOTH SEXES)
-# W = 10^(-4.68088 + 2.88534 * log10(L))# 
-# W = 10^(-4.68088) * 10^(2.88534 * log10(L))
-# W = 10^(-4.68088) * (10^log10(L))^2.88534
-# W = 10^(-4.68088) * L^2.88534
-# W = 0.00002085067 * L^2.88534
+save(file="../../RObjects/TTR_AllDietWeight.RData", ttrDiet) 
 
-# L = c(121,240)
-# 10^(-4.68088 + 2.88534 * log10(L))# 
-# 10^(-4.68088) * 10^(2.88534 * log10(L))
-# 10^(-4.68088) * (10^log10(L))^2.88534
-# 10^(-4.68088) * L^2.88534
-# W = 0.00002085067 * L^2.88534
-# 0.0718 * W
-# 0.000001497 * L^2.88534
