ad563aefa3cae27ead72043f27fdf7550e297b66
milokmilo <milokmilo@gmail.com>
2015-04-27 19:54:51 +0200

    Talla peso recalculada con funciones exponenciales y objetos renombrados para el cálculo de los pesos de referencia para Gadget

--- Length Weight/finalCode/TTR_LenWei.r
+++ Length Weight/finalCode/TTR_LenWei.r
@@ -36,9 +36,14 @@ mytheme <- theme(panel.background =  element_rect(fill = NA, colour = "black", s
 ###########     Length-Weight relationship  (Pierce et al., 2005)    #############
 
 
-log W = 5.597 + 3.2692 log L (R2=0.711)
-
-
+#log W = -5.597 + 3.2692 log L (R2=0.711)
+#log W = -5.597 + log L^3.2692
+#10^log W = 10^(-5.597 + log L^3.2692)
+#W = 10^(-5.597) * 10^(log L^3.2692)
+#W = 10^(-5.597) * L^3.2692
+#W = 0.00000259298 + L^3.2692
+LWallPierce <- c(0.00000259298, 3.2692)
+save(LWallPierce, file="../../RObjects/LWallPierce.RData")
 
 
 ###########     Length-Weight relationship  (Ridgway and Fenner, 1982)    #############
@@ -49,16 +54,16 @@ log W = 5.597 + 3.2692 log L (R2=0.711)
 
   # All
 
-LAll <- c(188,259)
-WAll <- c(85,184)
-Alm <- lm(WAll ~ LAll)
-All <- data.frame(L=LAll, W=WAll)
-#Allem <- nls(W ~ exp(a + b * L), data = All, start = list(a = 2.7, b = 0.01))
-All <- data.frame(LAll=90:350)
-All$WAll <- predict(Alm, newdata=All)
-#All$WAll <- predict(Allem, newdata=All)
-plot(LAll,WAll,ylim=c(0,300),xlim=c(90,350))
-lines(All)
+#LAll <- c(188,259)
+#WAll <- c(85,184)
+#Alm <- lm(WAll ~ LAll)
+#All <- data.frame(L=LAll, W=WAll)
+##Allem <- nls(WAll ~ (a * LAll^b), data = All, start = list(a = 0.01, b = 2))
+#All <- data.frame(LAll=90:350)
+#All$WAll <- predict(Alm, newdata=All)
+##All$WAll <- predict(Allem, newdata=All)
+#plot(LAll,WAll,ylim=c(0,300),xlim=c(90,350))
+#lines(All)
 
 
   # Males:
@@ -66,15 +71,17 @@ lines(All)
 LMales <- c(187,195,200,202,213,217,217,222,227,230,247,243)
 WMales <- c(93,101,103,99,116,119,118,121,132,158,158,182)
 M <- data.frame(LMales=LMales, WMales=WMales)
-Mem <- nls(WMales ~ exp(a + b * LMales), data = M, start = list(a = 4, b = 0.003))
+#Mem <- nls(WMales ~ exp(a + b * LMales), data = M, start = list(a = 4, b = 0.003))
+Mem <- nls(WMales ~ (a * LMales^b), data = M, start = list(a = -10, b = 3))
 Males <- data.frame(LMales=90:350)
 Males$WMales <- predict(Mem, newdata=Males)
 #plot(LMales,WMales,ylim=c(0,300),xlim=c(90,350))
 #lines(Males)
 #summary(Mem)
-#coef(Mem)
+LWmalesRF <- coef(Mem)
 # a          b 
-# 2.38196812 0.01119829 
+# 0.0002219646 2.4596457684 
+save(LWmalesRF, file="../../RObjects/LWmalesRF.RData")
 
 
   # Females:
@@ -82,15 +89,17 @@ Males$WMales <- predict(Mem, newdata=Males)
 LFemales <- c(194,205,205,207,223,224,228,219,239,223,234,249,250,244,252,256)
 WFemales <- c(98,110,105,105,118,121,131,143,142,171,157,184,173,158,161,155)
 F <- data.frame(LFemales=LFemales, WFemales=WFemales)
-Fem <- nls(WFemales ~ exp(a + b * LFemales), data = F, start = list(a = 4, b = 0.003))
+#Fem <- nls(WFemales ~ exp(a + b * LFemales), data = F, start = list(a = 4, b = 0.003))
+Fem <- nls(WFemales ~ (a * LFemales^b), data = F, start = list(a = -10, b = 3))
 Females <- data.frame(LFemales=90:350)
 Females$Wfemales <- predict(Fem, newdata=Females)
 #plot(LFemales,WFemales,ylim=c(0,300),xlim=c(90,350))
 #lines(Females)
 #summary(Fem)
-#coef(Fem)
+LWfemalesRF <- coef(Fem)
 # a           b 
-# 2.971140356 0.008564414 
+# 0.003172785 1.967658822 
+save(LWfemalesRF, file="../../RObjects/LWfemalesRF.RData")
 
 
   # All2
@@ -98,16 +107,18 @@ Females$Wfemales <- predict(Fem, newdata=Females)
 LAll2 <- c(LMales,LFemales)
 WAll2 <- c(WMales,WFemales)
 A <- data.frame(LAll2=LAll2, WAll2=WAll2)
-Aem <- nls(WAll2 ~ exp(a + b * LAll2), data = A, start = list(a = 4, b = 0.003))
+#Aem <- nls(WAll2 ~ exp(a + b * LAll2), data = A, start = list(a = 4, b = 0.003))
+Aem <- nls(WAll2 ~ (a * LAll2^b), data = A, start = list(a = -10, b = 3))
 All2 <- data.frame(LAll2=90:350)
 #All2$WAll2 <- predict(Alm2, newdata=All2)
 All2$WAll2 <- predict(Aem, newdata=All2)
 #plot(LAll2,WAll2,ylim=c(0,300),xlim=c(90,350))
 #lines(All2)
 #summary(Aem)
-#coef(Aem)
+LWallRF <- coef(Aem)
 # a           b 
-# 2.763609038 0.009463226 
+# 0.001232174 2.141609659 
+save(LWallRF, file="../../RObjects/LWallRF.RData")
 
 
 ### Plotting Length Weight following Ridgway and Fenner 1982
@@ -171,16 +182,17 @@ AWMales$weight <- predict(vbWM, newdata=AWMales)
 
 # Fit exponential model for length/weight and predict values
 ALWMales <- cbind(ALMales, weight=AWMales$weight)
-Mem <- nls(weight ~ exp(a + b * length), data=ALWMales, start = list(a = 4, b = 0.003))
-LWmales <- coef(Mem)
-# a          b 
-# 1.50137935 0.01518888 
+#Mem <- nls(weight ~ exp(a + b * length), data=ALWMales, start = list(a = 4, b = 0.003))
+Mem <- nls(weight ~ (a * length^b), data=ALWMales, start = list(a = 1, b = 1),
+           nls.control(maxiter = 500, tol = 1e-05, minFactor = 1/1024))
+LWmalesWS <- coef(Mem)
+# a                 b 
+# 0.0000005322137 3.578987 
 Mmod <- data.frame(length=seq(90,300)) 
 Mmod$weight <- predict(Mem,newdata=Mmod) 
 #plot(x=ALWMales$length,y=ALWMales$weight,xlim=c(100,300),ylim=c(0,350))
 #lines(Mmod, col="blue")
-
-save(LWmales, file="../../RObjects/LWmales.RData")
+save(LWmalesWS, file="../../RObjects/LWmalesWS.RData")
 
 
   # Females
@@ -199,16 +211,16 @@ AWFemales$weight <- predict(vbWF, newdata=AWFemales)
 
 # Fit exponential model for length/weight and predict values
 ALWFemales <- cbind(ALFemales, weight=AWFemales$weight)
-Fem <- nls(weight ~ exp(a + b * length), data=ALWFemales, start = list(a = 4, b = 0.003))
-LWfemales <- coef(Fem)
-# a          b 
-# 1.78346257 0.01404042
+#Fem <- nls(weight ~ exp(a + b * length), data=ALWFemales, start = list(a = 4, b = 0.003))
+Fem <- nls(weight ~ (a * length^b), data=ALWFemales, start = list(a = -10, b = 3))
+LWfemalesWS <- coef(Fem)
+# a              b 
+# 0.000008040019 3.082574
 Fmod <- data.frame(length=seq(100,300)) 
 Fmod$weight <- predict(Fem,newdata=Fmod) 
 #plot(x=ALWFemales$length, y=ALWFemales$weight, xlim=c(100,300), ylim=c(0,350))
 #lines(Fmod, col="red")
-
-save(LWfemales, file="../../RObjects/LWfemales.RData")
+save(LWfemalesWS, file="../../RObjects/LWfemalesWS.RData")
 
 
   # All
@@ -219,25 +231,49 @@ ALWFemales$sex <- "F"
 ALW <- rbind(ALWMales, ALWFemales)
 
 # Fit exponential model for length/weight and predict values
-Aem <- nls(weight ~ exp(a + b * length), data=ALW, start = list(a = 4, b = 0.003))
-LWall <- coef(Aem)
+#Aem <- nls(weight ~ exp(a + b * length), data=ALW, start = list(a = 4, b = 0.003))
+Aem <- nls(weight ~ (a * length^b), data=ALW, start = list(a = 1, b = 1),
+           nls.control(maxiter = 500, tol = 1e-05, minFactor = 1/1024))
+LWallWS <- coef(Aem)
 #        a          b 
-# 1.54827648 0.01500401
+# 0.0000004981065 3.589617
 Amod <- data.frame(length=seq(100,300))
 Amod$weight <- predict(Aem,newdata=Amod)  
 #plot(x=ALW$length, y=ALW$weight, xlim=c(100,300), ylim=c(0,350))
 #lines(Amod, col="green")
+save(LWallWS, file="../../RObjects/LWallWS.RData")
 
-save(LWall, file="../../RObjects/LWall.RData")
 
-# File for the model length-weight from 
-#load("../../RObjects/LWall.RData")
-#len <- seq(100.5, 250.5)
-#wei <- exp(LWall[[1]] + LWall[[2]] * len)
+############################# 4Gadget #####################################
+
+# File for the model length-weight from Pierce
+#load("../../RObjects/LWallPierce.RData")
+#len <- seq(100.5, 254.5)
+#wei <- LWallPierce[[1]] * len^LWallPierce[[2]]
 #data.frame(len, wei)
-#len <- seq(100.5, 250.5, by=5)
-#wei <- exp(LWall[[1]] + LWall[[2]] * len)
+#len <- seq(100.5, 255.5, by=5)
+#wei <- LWallPierce[[1]] * len^LWallPierce[[2]]
 #data.frame(len, wei)
+
+# File for the model length-weight from Ridgway and Fenner, 1982
+#load("../../RObjects/LWallRF.RData")
+#len <- seq(100.5, 254.5)
+#wei <- LWallRF[[1]] * len^LWallRF[[2]]
+#data.frame(len, wei)
+#len <- seq(100.5, 255.5, by=5)
+#wei <- LWallRF[[1]] * len^LWallRF[[2]]
+#data.frame(len, wei)
+
+# File for the model length-weight from Wells and scott, 1999
+#load("../../RObjects/LWallWS.RData")
+#len <- seq(100.5, 254.5)
+#wei <- LWallWS[[1]] * len^LWallWS[[2]]
+#data.frame(len, wei)
+#len <- seq(100.5, 255.5, by=5)
+#wei <- LWallWS[[1]] * len^LWallWS[[2]]
+#data.frame(len, wei)
+
+####################################################################### #
 
 
 
